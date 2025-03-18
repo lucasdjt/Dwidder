4ae16e74d78d39039020b8f35838533f56b949ab
Lucas DJT <draggaspro@gmail.com>
2025-03-18 12:28:11 +0100

    Ajout de la sécurité des Servlet en BASIC

--- WEB-INF/src/controleur/APIServlet.java
+++ WEB-INF/src/controleur/APIServlet.java
@@ -14,6 +14,7 @@ import modele.dto.User;
 import modele.dao.LogsDAO;
 import utils.BAO;
 import java.io.PrintWriter;
+import java.util.Base64;
 import java.util.List;
 
 @WebServlet("/api/*")
@@ -22,12 +23,12 @@ public class APIServlet extends HttpServlet {
      * API :
      * - /api/post : liste des posts publics
      * > curl -i -X GET http://localhost:8080/ReseauSocial/api/post
-     * - /api/post/{uid} : liste des posts reponses au post avec id = {id}
-     * > curl -i -X GET http://localhost:8080/ReseauSocial/api/post/1
-     * - /api/user/{idPseudo} : liste des posts publics avec idpseudo = {idpseudo}
-     * > curl -i -X GET http://localhost:8080/ReseauSocial/api/user/draggas
+     * - /api/post/{uid} : un post en particulier à moi
+     * > curl -i -x GET -u draggas:draggas GET http://localhost:8080/ReseauSocial/api/post/1
+     * - /api/user : liste de nos posts
+     * > curl -i -X GET -u draggas:draggas http://localhost:8080/ReseauSocial/api/user
      * - /api/group/{gid} : liste des posts d'un groupe données
-     * > curl -i -X GET http://localhost:8080/ReseauSocial/api/group/1
+     * > curl -i -X GET -u draggas:draggas http://localhost:8080/ReseauSocial/api/group/1
      */
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
         throws ServletException, IOException {
@@ -58,11 +59,29 @@ public class APIServlet extends HttpServlet {
             switch (resource) {
                 case "post":
                     if (pathParts.length == 3) {
+                        String authorization = req.getHeader("Authorization");
+                        if (authorization == null || !authorization.startsWith("Basic")){
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid");
+                            return;
+                        }
+                        String token = authorization.substring("Basic".length()).trim();
+                        byte[] base64 = Base64.getDecoder().decode(token);
+                        String[] values = (new String(base64)).split(":");
+
+                        if (values.length != 2 || !isValidUser(values[0], values[1])) {
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+                            return;
+                        }
+
                         int postId = Integer.parseInt(pathParts[2]);
                         PostsDAO postsDAO = new PostsDAO();
                         PostDetails p = postsDAO.findPostDetails(postId);
                         if (p == null) {
                             res.sendError(HttpServletResponse.SC_NOT_FOUND, "Post does not exist");
+                            return;
+                        }
+                        if(!p.getIdPseudo().equals(values[0])){
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not your post");
                             return;
                         }
                         out.print(BAO.toJson(p));
@@ -82,11 +101,24 @@ public class APIServlet extends HttpServlet {
                     }
                     break;
                 case "user":
-                    if (pathParts.length == 3) {
-                        String userID = pathParts[2];
+                    if (pathParts.length == 2) {
+                        String authorization = req.getHeader("Authorization");
+                        if (authorization == null || !authorization.startsWith("Basic")){
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid");
+                            return;
+                        }
+                        String token = authorization.substring("Basic".length()).trim();
+                        byte[] base64 = Base64.getDecoder().decode(token);
+                        String[] values = (new String(base64)).split(":");
+
+                        if (values.length != 2 || !isValidUser(values[0], values[1])) {
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+                            return;
+                        }
+
                         UsersDAO usersDAO = new UsersDAO();
-                        User u = usersDAO.findUserByPseudo(userID);
-                        if(u == null){
+                        User u = usersDAO.findUserByPseudo(values[0]);
+                        if (u == null) {
                             res.sendError(HttpServletResponse.SC_NOT_FOUND, "User does not exist");
                             return;
                         }
@@ -107,11 +139,30 @@ public class APIServlet extends HttpServlet {
                     break;
                 case "group":
                     if (pathParts.length == 3) {
+                        String authorization = req.getHeader("Authorization");
+                        if (authorization == null || !authorization.startsWith("Basic")){
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid");
+                            return;
+                        }
+                        String token = authorization.substring("Basic".length()).trim();
+                        byte[] base64 = Base64.getDecoder().decode(token);
+                        String[] values = (new String(base64)).split(":");
+
+                        if (values.length != 2 || !isValidUser(values[0], values[1])) {
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+                            return;
+                        }
+
                         int gid = Integer.parseInt(pathParts[2]);
                         GroupesDAO gDao = new GroupesDAO();
                         PostsDAO dao = new PostsDAO();
+                        UsersDAO uDao = new UsersDAO();
                         if(gDao.findGroupByGid(gid) == null){
                             res.sendError(HttpServletResponse.SC_NOT_FOUND, "Group does not exist");
+                            return;
+                        }
+                        if(!gDao.getListUsersOfAGroup(gid).contains(uDao.findUserByPseudo(values[0]))){
+                            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not part of the group");
                             return;
                         }
                         List<PostDetails> posts = dao.getListPostsOfGroup(gid, true);
@@ -139,5 +190,14 @@ public class APIServlet extends HttpServlet {
         } catch (IllegalAccessException e) {
             res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error occurred");
         }
+    }
+
+    private boolean isValidUser(String idPseudo, String password) {
+        UsersDAO usersDAO = new UsersDAO();
+        User user = usersDAO.findUserByPseudo(idPseudo);
+        if (user == null) {
+            return false;
+        }
+        return user.getMdp().equals(password);
     }
 }
